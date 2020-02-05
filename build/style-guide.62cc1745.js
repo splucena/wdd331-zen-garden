6e7546de6885568a1feecc63b9cad9f00c3cd63f
splucena <sumilangplucena@yahoo.com.ph>
2020-02-05 23:16:02 +0800

    Added template and gitignore.

--- build/style-guide.62cc1745.js
+++ build/style-guide.62cc1745.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37994" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36097" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
