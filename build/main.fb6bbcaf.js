b6f6fa1ceb5d714e70011b20282b601923c9a8ec
splucena <sumilangplucena@yahoo.com.ph>
2020-02-08 14:43:58 +0800

    Added partials, updated index.html

--- build/main.fb6bbcaf.js
+++ build/main.fb6bbcaf.js
@@ -248,7 +248,7 @@ function updateNavigation() {
     }
   });
 }
-},{}],"../../../../AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -276,11 +276,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39461" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53522" + '/');
->>>>>>> 2f7500aa1898120d271f5f8d01b2a04b8f669d70
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39876" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -456,5 +452,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js","js/main.js"], null)
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/main.js"], null)
 //# sourceMappingURL=/main.fb6bbcaf.js.map
